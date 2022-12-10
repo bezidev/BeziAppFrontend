5bff48f04a37dd315c7d7d9606cb19fc2421ec63
mytja <mytja@protonmail.com>
2022-12-10 21:40:02 +0100

    touch gestures

--- src/App.svelte
+++ src/App.svelte
@@ -12,6 +12,26 @@
 	let pathname = window.location.pathname;
 	let unsub;
 	onMount(() => {
+		let touchstartX = 0;
+		let touchendX = 0
+		const distance = 110;
+
+		function checkDirection() {
+			console.log(touchstartX, touchendX)
+			if (touchendX < touchstartX && distance < Math.abs(touchstartX-touchendX)) open = false
+			if (touchendX > touchstartX && distance < Math.abs(touchstartX-touchendX) && touchstartX - 20 < 0) open = true
+		}
+
+		document.addEventListener('touchstart', e => {
+			touchstartX = e.changedTouches[0].screenX
+		})
+
+		document.addEventListener('touchend', e => {
+			touchendX = e.changedTouches[0].screenX
+			checkDirection()
+		})
+
+
 		unsub = globalHistory.listen(({ location, action }) => {
 			console.log(location, action);
 			pathname = location.pathname;
@@ -27,7 +47,7 @@
 		<TopAppBar variant="static" style="background-color: rgba(0, 128, 83, 1);">
 			<Row>
 				<Section style="display: flex; flex-direction: row; align-items: center;">
-					<IconButton style="margin: 0 0 0 0.5em;" class="material-icons" on:click={() => open=!open}>menu</IconButton>
+					<IconButton style="margin: 0 0 0 0.5em;" class="material-icons" on:click={() => open=!open}>{#if open}close{:else}menu{/if}</IconButton>
 					<Title>BežiApp</Title>
 				</Section>
 				<Section align="end" toolbar>
@@ -42,7 +62,7 @@
 			</Row>
 		</TopAppBar>
 	{/if}
-	<div class="drawer-container">
+	<div class="drawer-container" id="router">
 		<Drawer open={open} statusFunction={(o) => open=o} />
 		<AppContent class="app-content">
 			<main class="main-content">
