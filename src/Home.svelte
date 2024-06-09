586caa3902828986984ad258289bede7d54e2756
mytja <mitja@severkar.eu>
2024-06-09 18:16:46 +0200

    opozorilo

--- src/Home.svelte
+++ src/Home.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
     import Timetable from "./Widgets/Timetable.svelte";
     import {handleRejection} from "./constants";
+    import {marked} from "marked";
 
     const token = localStorage.getItem("key");
     if (token === null || token === undefined || token === "") {
@@ -16,6 +17,39 @@
         window.location.href = "/login";
     }
 </script>
+
+<div style="padding: 10px; background-color: darkorange; border-radius: 10px;">
+    <h2>Pomembno obvestilo</h2>
+    Kot ste nekateri že lahko videli, se s prihodnjim šolskim letom poslavlja šolski sistem GimSIS. Nadomestil ga bo eAsistent.
+
+    <p/>
+
+    Ja, ta eAsistent, ki je mnogim poznan že iz osnovnih šol. Točno ta eAsistent, ki zahteva premium paket za vpogled v vaše ocene, ocenjevanja in druge osnovne informacije. Ta eAsistent, ki ima grozno aplikacijo s povprečno oceno aplikacija 1,3 na Google Play trgovini (zelo slabo).
+
+    <p/>
+
+    Ker je BežiApp direktno odvisen od podatkov iz GimSISa, imam dve možnosti – ga ali zapreti ali pa prilagoditi za eAsistent.
+
+    <p/>
+
+    Ker tudi mene grozljivo skrbi kvaliteta eAsistentove aplikacije mojAsistent, sem se odločil za slednje. BežiApp bo ostal v taki obliki, v kakršni ga poznate zdaj, v ozadju pa se bo vse spremenilo ... na žalost na slabše za vas, a temu se lahko zahvalite zgolj eŠoli – matičnemu podjetju eAsistenta.
+
+    <p/>
+
+    Potrebna bo ponovna registracija uporabniškega računa, k čemu vas bo v prihodnjem letu pozval tudi BežiApp. Za zdaj pa uživajte v zadnjih dneh GimSISa, preden na našo šolo vstopi požrešna alternativa.
+
+    <p/>
+
+    Hvala za vse. Lepe počitnice vam želi
+
+    <p/>
+
+    Mitja, 2. A (kmalu 3. A)
+
+    <p/>
+</div>
+
+<p/>
 
 <Timetable />
 
--- src/Login.svelte
+++ src/Login.svelte
@@ -72,64 +72,89 @@
         </Actions>
     </Snackbar>
     <div class="center">
-            <h1>Prijava v {#if loginType === "gimsis"}BežiApp (GimSIS){:else}{#if loginType === "account"}BežiApp{:else}Lo.Polis{/if}{/if}</h1>
-            <Textfield type="text" bind:value={email} label="Uporabniško ime" style="width: 80%;" helperLine$style="width: 80%;">
-                <HelperText slot="helper" style="left: 12%; position: relative;">GimSIS uporabniško ime</HelperText>
-                <Icon class="material-icons" slot="leadingIcon">person</Icon>
-            </Textfield>
-            <p />
-            <Textfield on:keypress={(key) => {
-                if (key.key === "Enter") {
-                    login();
-                }
-            }} type={passwordVisibility ? "text" : "password"} bind:value={password} label="Geslo" style="width: 80%;" helperLine$style="width: 80%;">
-                <IconButton slot="trailingIcon" on:click={() => passwordVisibility = !passwordVisibility}>
-                    {#if passwordVisibility}
-                        <svg width="24" height="24" fill="none" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="M2.22 2.22a.75.75 0 0 0-.073.976l.073.084 4.034 4.035a9.986 9.986 0 0 0-3.955 5.75.75.75 0 0 0 1.455.364 8.49 8.49 0 0 1 3.58-5.034l1.81 1.81A4 4 0 0 0 14.8 15.86l5.919 5.92a.75.75 0 0 0 1.133-.977l-.073-.084-6.113-6.114.001-.002-6.95-6.946.002-.002-1.133-1.13L3.28 2.22a.75.75 0 0 0-1.06 0ZM12 5.5c-1 0-1.97.148-2.889.425l1.237 1.236a8.503 8.503 0 0 1 9.899 6.272.75.75 0 0 0 1.455-.363A10.003 10.003 0 0 0 12 5.5Zm.195 3.51 3.801 3.8a4.003 4.003 0 0 0-3.801-3.8Z" fill="#ffffff"/></svg>
-                    {:else}
-                        <svg width="24" height="24" fill="none" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="M12 9.005a4 4 0 1 1 0 8 4 4 0 0 1 0-8ZM12 5.5c4.613 0 8.596 3.15 9.701 7.564a.75.75 0 1 1-1.455.365 8.503 8.503 0 0 0-16.493.004.75.75 0 0 1-1.455-.363A10.003 10.003 0 0 1 12 5.5Z" fill="#ffffff"/></svg>
-                    {/if}
-                </IconButton>
-                <HelperText slot="helper" style="left: 12%; position: relative;">GimSIS geslo, razen če ste si spremenili geslo BežiApp računa</HelperText>
-                <Icon class="material-icons" slot="leadingIcon">key</Icon>
-            </Textfield>
-            <p />
-            Če ste si spremenili GimSIS geslo, pred tem pa ste se že prijavili v BežiApp, se v BežiApp še vedno prijavljate s starim GimSIS geslom.
-            Če prijava ne deluje, kontaktirajte razvijalca na <a href="mailto:mitja.severkar@gimb.org">mitja.severkar@gimb.org</a>.
-            <p />
-            <Button on:click={async () => await login()} variant="raised">
-                <Label>PRIJAVA</Label>
-            </Button>
-            <p/>
-            Prijava ne deluje? Ponastavite uporabniški profil.
-            <p/>
-            <Button on:click={() => navigate("/reset")} variant="outlined">
-                <Label>PONASTAVITEV</Label>
-            </Button>
+        <div style="padding: 10px; background-color: darkorange; border-radius: 10px;">
+            <h2>Pomembno obvestilo</h2>
+            Dragi bodoči prvošolci!
 
-            {#if loginType === "account"}
-                <p/>
-                Ob kliku na gumb Prijava, se vam bo avtomatično ustvaril BežiApp račun, ki olajša prijavo v BežiApp sistem in omogoči dodatne funkcije.
-                S klikom na gumb se prav tako strinjate s <a href="/tos.html">pogoji uporabe storitve BežiApp</a>.<br>
-                BežiApp uporablja minimalno in neinvazivno analitiko ter statistiko z uporabo <a href="https://stats.severkar.eu">Plausible analitike</a>.
-            {/if}
-            {#if loginType === "gimsis"}
-                <p/>
-                <Notification/>
-            {/if}
             <p/>
-            <LayoutGrid>
-                {#each notifications as notification}
-                    {#if notification.notification_type === "gimsis_closed" || notification.notification_type === "login"}
-                        <Cell span={12 / Math.floor(95 / Math.max(width / 700, 1))}>
-                            <div style="padding: 10px; background-color: darkorange; border-radius: 10px;">
-                                <h2>{notification.name}</h2>
-                                {@html insane(marked(notification.description))}
-                            </div>
-                        </Cell>
-                    {/if}
-                {/each}
-            </LayoutGrid>
+
+            Če opažate to aplikacijo, še malo potrpite s prijavo. V prihodnjem šolskem letu, 2024/2025, bo Gimnazija Bežigrad na žalost vseh zamenjala svoj prejšnji šolski informacijski sistem, GimSIS, z eAsistentom. In ne, to ni všeč ne meni, ne drugim.
+
+            <p/>
+
+            Ker je BežiApp osnovan na GimSISu in direktno črpa podatke iz tam, moram na novo napisati celotno zaledje, kar pa pri velikosti, kakršno je dosegel BežiApp vsekakor ni lahko. Obljubljam pa, da bo do zadnjega tedna pred začetkom šole BežiApp ponovno odprt za vse.
+
+            <p/>
+
+            Do takrat pa vam lepe počitnice želi
+
+            <p/>
+
+            BežiApp ekipa
+
+            <p/>
+        </div>
+
+        <p/>
+
+        <h1>Prijava v {#if loginType === "gimsis"}BežiApp (GimSIS){:else}{#if loginType === "account"}BežiApp{:else}Lo.Polis{/if}{/if}</h1>
+        <Textfield type="text" bind:value={email} label="Uporabniško ime" style="width: 80%;" helperLine$style="width: 80%;">
+            <HelperText slot="helper" style="left: 12%; position: relative;">GimSIS uporabniško ime</HelperText>
+            <Icon class="material-icons" slot="leadingIcon">person</Icon>
+        </Textfield>
+        <p />
+        <Textfield on:keypress={(key) => {
+            if (key.key === "Enter") {
+                login();
+            }
+        }} type={passwordVisibility ? "text" : "password"} bind:value={password} label="Geslo" style="width: 80%;" helperLine$style="width: 80%;">
+            <IconButton slot="trailingIcon" on:click={() => passwordVisibility = !passwordVisibility}>
+                {#if passwordVisibility}
+                    <svg width="24" height="24" fill="none" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="M2.22 2.22a.75.75 0 0 0-.073.976l.073.084 4.034 4.035a9.986 9.986 0 0 0-3.955 5.75.75.75 0 0 0 1.455.364 8.49 8.49 0 0 1 3.58-5.034l1.81 1.81A4 4 0 0 0 14.8 15.86l5.919 5.92a.75.75 0 0 0 1.133-.977l-.073-.084-6.113-6.114.001-.002-6.95-6.946.002-.002-1.133-1.13L3.28 2.22a.75.75 0 0 0-1.06 0ZM12 5.5c-1 0-1.97.148-2.889.425l1.237 1.236a8.503 8.503 0 0 1 9.899 6.272.75.75 0 0 0 1.455-.363A10.003 10.003 0 0 0 12 5.5Zm.195 3.51 3.801 3.8a4.003 4.003 0 0 0-3.801-3.8Z" fill="#ffffff"/></svg>
+                {:else}
+                    <svg width="24" height="24" fill="none" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="M12 9.005a4 4 0 1 1 0 8 4 4 0 0 1 0-8ZM12 5.5c4.613 0 8.596 3.15 9.701 7.564a.75.75 0 1 1-1.455.365 8.503 8.503 0 0 0-16.493.004.75.75 0 0 1-1.455-.363A10.003 10.003 0 0 1 12 5.5Z" fill="#ffffff"/></svg>
+                {/if}
+            </IconButton>
+            <HelperText slot="helper" style="left: 12%; position: relative;">GimSIS geslo, razen če ste si spremenili geslo BežiApp računa</HelperText>
+            <Icon class="material-icons" slot="leadingIcon">key</Icon>
+        </Textfield>
+        <p />
+        Če ste si spremenili GimSIS geslo, pred tem pa ste se že prijavili v BežiApp, se v BežiApp še vedno prijavljate s starim GimSIS geslom.
+        Če prijava ne deluje, kontaktirajte razvijalca na <a href="mailto:mitja.severkar@gimb.org">mitja.severkar@gimb.org</a>.
+        <p />
+        <Button on:click={async () => await login()} variant="raised">
+            <Label>PRIJAVA</Label>
+        </Button>
+        <p/>
+        Prijava ne deluje? Ponastavite uporabniški profil.
+        <p/>
+        <Button on:click={() => navigate("/reset")} variant="outlined">
+            <Label>PONASTAVITEV</Label>
+        </Button>
+
+        {#if loginType === "account"}
+            <p/>
+            Ob kliku na gumb Prijava, se vam bo avtomatično ustvaril BežiApp račun, ki olajša prijavo v BežiApp sistem in omogoči dodatne funkcije.
+            S klikom na gumb se prav tako strinjate s <a href="/tos.html">pogoji uporabe storitve BežiApp</a>.<br>
+            BežiApp uporablja minimalno in neinvazivno analitiko ter statistiko z uporabo <a href="https://stats.severkar.eu">Plausible analitike</a>.
+        {/if}
+        {#if loginType === "gimsis"}
+            <p/>
+            <Notification/>
+        {/if}
+        <p/>
+        <LayoutGrid>
+            {#each notifications as notification}
+                {#if notification.notification_type === "gimsis_closed" || notification.notification_type === "login"}
+                    <Cell span={12 / Math.floor(95 / Math.max(width / 700, 1))}>
+                        <div style="padding: 10px; background-color: darkorange; border-radius: 10px;">
+                            <h2>{notification.name}</h2>
+                            {@html insane(marked(notification.description))}
+                        </div>
+                    </Cell>
+                {/if}
+            {/each}
+        </LayoutGrid>
         <p/>
     </div>
 </main>
